50d9c2b21c054ca7a11905f6e0f27eea48a1dbef
James Bint <khakimonk@gmail.com>
2016-05-15 21:38:11 +0100

    Final submission

--- JS/weather.js
+++ JS/weather.js
@@ -1,10 +1,30 @@
-var api = "api.openweathermap.org/data/2.5/weather?";
-var key = "264176e7a08a5448d6a929a45b47a202";
+$(function () {
 
-var getLoc = $.getJSON("http://ipinfo.io/json?callback=JSON_CALLBACK");
+  var $weather = $('#display');
+  var $img = $('#img');
 
-
-
-$(document).ready(function() {
-    $('.jumbotron').text(getLoc);
+  $.ajax({
+    type: 'GET',
+    url: "http://api.wunderground.com/api/d4d45ef6cba449be/geolookup/conditions/q/autoip.json",
+    dataType: "jsonp",
+    success: function(data) {
+      var temp_c = data['current_observation']['temp_c'];
+      var location = data['location']['city'];
+      var temp_f = data['current_observation']['temp_f'];
+      var icon = data['current_observation']['icon_url'];
+      var tempDisplay = temp_c;
+      $weather.html('<p>Current Temperature: ' + tempDisplay + 'c <br>City: ' + location + '</p>');
+      $img.html('<img src="' + icon + '"/>');
+      $('#btn').click(function() {
+        if(tempDisplay == temp_c) {
+          tempDisplay = temp_f;
+          $weather.html('<p>Current Temperature: ' + tempDisplay + 'f <br>City: ' + location + '</p>');
+        }
+        else {
+          tempDisplay = temp_c;
+          $weather.html('<p>Current Temperature: ' + tempDisplay + 'c <br>City: ' + location + '</p>');
+        }
+      });
+    }
+  });
 });
