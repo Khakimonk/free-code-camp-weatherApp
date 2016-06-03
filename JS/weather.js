d3aa0a3de450d8efbe357ec8d17b52769c8fcbb2
James Bint <Khakimonk@gmail.com>
2016-06-03 23:24:14 +0100

    updated to not use bootstrap

--- JS/weather.js
+++ JS/weather.js
@@ -1,6 +1,7 @@
 var $weather = $('#display');
 var $img = $('#img');
 var $changeTemp = $('#btn');
+var $showForecast = $('#forecastBtn');
 
 function getCurrentWeather() {
     $.ajax({
@@ -19,7 +20,7 @@ function getForecastWeather() {
         url: "http://api.wunderground.com/api/d4d45ef6cba449be/geolookup/forecast/q/autoip.json",
         dataType: "jsonp",
         success: function(data) {
-            console.log(data);
+            storeForecast(data);
         }
     });
 }
@@ -29,7 +30,7 @@ function storeData(data) {
     var location = data['location']['city'];
     var temp_f = data['current_observation']['temp_f'];
     var tempDisplay = temp_c;
-    $weather.append('<p>Current Temperature: ' + tempDisplay + 'c <br>City: ' + location + '</p>');
+    $weather.append('<div class="result"><p>Current Temperature: ' + tempDisplay + 'c <br>City: ' + location + '</p></div>');
     $changeTemp.click(function() {
         $weather.empty();
         showCurrent(temp_c, temp_f, location, tempDisplay);
@@ -50,5 +51,44 @@ function showCurrent(temp_c, temp_f, location, tempDisplay) {
     });
 }
 
+function storeForecast(data) {
+  var forecast = data['forecast']['simpleforecast']['forecastday'];
+  console.log(forecast);
+  var condition = [];
+  var forecastDay = [];
+  var low_c = [];
+  var low_f = [];
+  var high_c = [];
+  var high_f = [];
+  for(var i = 0; i < forecast.length; i++) {
+    condition.push(forecast[i]['conditions']);
+    forecastDay.push(forecast[i]['date']['weekday_short']);
+    low_c.push(forecast[i]['low']['celsius']);
+    low_f.push(forecast[i]['low']['fahrenheit']);
+    high_c.push(forecast[i]['high']['celsius']);
+    high_f.push(forecast[i]['high']['fahrenheit']);
+  }
+  for(var i = 0; i < condition.length; i++) {
+    $weather.append('<div class="result"><p>' + forecastDay[i] + '<br>' + condition[i] + '<br>Low temp of ' + low_c[i] + 'c<br>High temp of ' + high_c[i] + 'c</p></div>');
+  }
+}
+
+function toggleDisplay() {
+  var setDisplay = 'current';
+  console.log(setDisplay);
+  $showForecast.click(function() {
+    if(setDisplay == 'current') {
+      $weather.empty();
+      setDisplay = 'forecast';
+      getForecastWeather();
+    }
+    else if(setDisplay == 'forecast') {
+      $weather.empty();
+      setDisplay = 'current';
+      getCurrentWeather();
+    }
+  });
+}
+
 getCurrentWeather();
-getForecastWeather();
+toggleDisplay();
